f448c36129e7c91e21d5146482996b4b000803b7
Luke Lazar <lukelazar@Lukes-MBP.modem>
2023-05-14 01:38:43 +1000

    More Comments

--- public/components/staticComponent.js
+++ public/components/staticComponent.js
@@ -1,5 +1,11 @@
-// import Circuit from "./circuit.js";
-
+/*  Class: StaticCircuitComponent:
+    *   This base class is simply the "clickable" version of each component that when clicked on by a user, it creates
+    *   the real component. The class simply needs the type of component it creates, so that the stylesheet can update
+    *   its image.
+    * 
+    *      Member functions:
+    *       - onClick: Tells the circuit to create the real required component
+*/
 class StaticCircuitComponent {
     constructor(id, circuit, type) {
         this.id = id;
@@ -21,6 +27,8 @@ class StaticCircuitComponent {
     }
 }
 
+//To define a new component pass the base class the type of component as a string, and define the 
+//virutal onClick function.
 class Resistor extends StaticCircuitComponent{
     constructor(id, circuit){
         super(id,circuit, "Resistor");
