4173d08d6bf3c55a1b337c5150510ccd49d3cef7
Luke Lazar <lukelazar@Lukes-MBP.modem>
2023-05-14 13:53:41 +1000

    Comments on Node and component class

--- public/components/nodes.js
+++ public/components/nodes.js
@@ -1,11 +1,21 @@
 import WireManager from "./wire.js";
 
+/*  Class: Node: 
+    *       This class defines the nodes for each component. Clicking on allows the user to create a wire that will connect 
+    *       any nodes touching the same wire.
+    *
+    *       
+    *      Member functions:
+    *       - OnClick(): creates a wire starting at the node
+*/
 class Node {
     constructor(x, y, component, wireManager, id) {
+        //Store the nodes unique ID to communicate with simulator
         this.id = id
         this.x = x;
         this.y = y;
 
+        //Create html element
         this.element = document.createElement("div");
         this.element.classList.add('node');
         component.element.appendChild(this.element);
@@ -17,6 +27,7 @@ class Node {
         this.element.addEventListener("mousedown", this.mouseDown.bind(this));
         this.element.addEventListener("mouseup", this.mouseUp.bind(this));
 
+        //Store the associated component and wire manager
         this.parent = component;
         this.wm = wireManager;
 
@@ -27,18 +38,20 @@ class Node {
     }
 
     updatePos(){
+        //gets the current position of the node (accounts for any rotation of the parent HTML element)
         const pos = this.element.getBoundingClientRect();
 
-        //Tell any wires attatched, that the node has moved
         this.x = Math.round(pos.x/20) * 20;
         this.y = Math.round(pos.y/20) * 20;
-        const nodeMove = new CustomEvent("node_move", {detail:{ pos: this.getPos() }});
-        this.element.dispatchEvent(nodeMove);
+        
+        //Tell any wires attatched, that the node has moved
+//        const nodeMove = new CustomEvent("node_move", {detail:{ pos: this.getPos() }});
+//        this.element.dispatchEvent(nodeMove);
     }
 
     mouseDown(event){
         event.stopPropagation();
-
+        //Tells the wiremanager to create a new wire starting at this node
 
         //Send node location to the wire Manager
         this.wm.Start(this);
@@ -55,7 +68,7 @@ class Node {
 
     listenNodeMove(wire){
         //Attach a wire to the node
-        this.element.addEventListener("node_move", wire.handleMove.bind(wire));
+        //this.element.addEventListener("node_move", wire.handleMove.bind(wire));
 
     }
 
