4a79be35f2da253f0fdd4a080e1ae9fc276eb5dc
Luke Lazar <74656988+lazarr1@users.noreply.github.com>
2026-01-01 21:13:45 +1100

    restored websocket main file

--- src/main_websocket.cpp
+++ src/main_websocket.cpp
@@ -2,12 +2,6 @@
 #include <boost/beast/websocket.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <iostream>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <queue>
-#include <atomic>
-#include <array>
 #include "simulator/router.h"
 
 namespace beast = boost::beast;             // from <boost/beast.hpp>
@@ -16,67 +10,62 @@ namespace websocket = beast::websocket;     // from <boost/beast/websocket.hpp>
 namespace net = boost::asio;                // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
 
-constexpr int MAX_SIM_THREADS = 5;
-std::mutex thread_mutex;
-std::condition_variable thread_cv;
-std::atomic<int> active_threads(0);
-
-void handle_websocket_session(tcp::socket socket, Router router) {
-    try {
-        beast::flat_buffer buffer;
-        http::request<http::string_body> req;
-        http::read(socket, buffer, req);
-
-        if (websocket::is_upgrade(req)) {
-            websocket::stream<tcp::socket> ws(std::move(socket));
-            ws.accept(req);
-
-            while (ws.is_open()) {
-                beast::flat_buffer buffer;
-                ws.read(buffer);
-                std::cout << "Received message: " << beast::make_printable(buffer.data()) << std::endl;
-                std::string message = beast::buffers_to_string(buffer.data());
-                router.RouteMessage(message);
-                if (router.GetFinishedStatus()) {
-                    ws.write(net::buffer(router.GetResponse()));
-                    ws.close(websocket::close_code::normal);
-                }
-            }
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Error in thread: " << e.what() << std::endl;
-    }
-    active_threads--;
-    thread_cv.notify_one();
-}
-
 int main(){
+
     Router router;
+    // The io_context is required for all I/O
     net::io_context ioc;
+
+    // Create and bind the acceptor to listen on TCP port 8080
     tcp::acceptor acceptor(ioc, {tcp::v4(), 8080});
     std::cout << "Listening on port " << acceptor.local_endpoint().port() << std::endl;
 
     while (true) {
+        // Wait for a new client connection
+
         try {
-            tcp::socket socket(ioc);
+	    tcp::socket socket(ioc);
             acceptor.accept(socket);
 
-            std::unique_lock<std::mutex> lock(thread_mutex);
-            if (active_threads >= MAX_SIM_THREADS) {
-                // Too many threads, respond with busy message
-                http::response<http::string_body> busy_resp{
-                    http::status::service_unavailable, 11};
-                busy_resp.set(http::field::content_type, "text/plain");
-                busy_resp.body() = "Busy too much traffi";
-                busy_resp.prepare_payload();
-                http::write(socket, busy_resp);
-                continue;
-            }
-            active_threads++;
-            std::thread(handle_websocket_session, std::move(socket), router).detach();
+            // Read the incoming request into a beast::flat_buffer
+	    beast::flat_buffer buffer;
+            http::request<http::string_body> req;
+	    http::read(socket, buffer, req);
+
+            // Create a WebSocket session
+	    if (websocket::is_upgrade(req)) {
+		websocket::stream<tcp::socket> ws(std::move(socket));
+		ws.accept(req);
+
+                while(ws.is_open()){
+                    // Receive a message
+                    beast::flat_buffer buffer;
+                    ws.read(buffer);
+    
+                    std::cout << "Received message: " << beast::make_printable(buffer.data()) << std::endl;
+    
+                    
+                    std::string message = beast::buffers_to_string(buffer.data());
+    
+                    router.RouteMessage(message);
+    
+    
+                    if(router.GetFinishedStatus()){
+                        ws.write(net::buffer(router.GetResponse()));
+                        ws.close(websocket::close_code::normal);
+                    }
+                }
+    	    }
+            //websocket::stream<tcp::socket> ws(std::move(socket));
+
+            // Accept the WebSocket handshake
+            // ws.accept();
+
+
         } catch (const std::exception& e) {
             std::cerr << "Error: " << e.what() << std::endl;
         }
     }
+
     return 0;
 }
