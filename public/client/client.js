e700ed8ec83a44f878eed76c41276a6892dfe369
luke pc <lazarluke02@gmail.com>
2026-02-15 16:19:32 +1100

    updated client with timeout

--- public/client/client.js
+++ public/client/client.js
@@ -1,6 +1,8 @@
 class Client{
 
     constructor(circuit){
+		this.responseTimeout = null;
+		this.TIMEOUT_MS = 10000; // 5 seconds
         this.circuit = circuit;
         this.socket = null;
         this.isConnecting = false;
@@ -14,7 +16,7 @@ class Client{
         if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
             wsHost = 'ws://localhost:8080';
         } else {
-            wsHost = 'wss://lulazar.com/ws';
+            wsHost = 'wss://lulazar.com/circuit/ws';
         }
         this.socket = new WebSocket(wsHost);
         this.isConnecting = true;
@@ -29,6 +31,10 @@ class Client{
         });
         this.socket.addEventListener('close', () => {
             this.isConnecting = false;
+
+			setTimeout(() => {
+				this.initialiseSocket();
+			}, 5000);
         });
         this.socket.addEventListener('error', () => {
             this.isConnecting = false;
@@ -38,6 +44,11 @@ class Client{
     
     ProcessServerMessage(event){
         const msg = event.data;
+
+		if (this.responseTimeout) {
+			clearTimeout(this.responseTimeout);
+			this.responseTimeout = null;
+		}
 
         if(msg != null){
             console.log(msg);
@@ -66,17 +77,43 @@ class Client{
         delete this.ProcessServerMessageBind;
     }
 
-    sendMsg(msg){
-        if (!this.socket || this.socket.readyState === this.socket.CLOSED) {
-            this.removeSocket();
-            this.initialiseSocket();
-            this.messageQueue.push(msg);
-        } else if (this.socket.readyState === this.socket.CONNECTING || this.isConnecting) {
-            this.messageQueue.push(msg);
-        } else if (this.socket.readyState === this.socket.OPEN) {
-            this.socket.send(msg);
-        }
-    }
+	startResponseTimeout(lastMsg){
+		// Clear any existing timeout
+		if (this.responseTimeout) {
+			clearTimeout(this.responseTimeout);
+		}
+
+		this.responseTimeout = setTimeout(() => {
+			console.warn("WebSocket response timeout. Reconnecting...");
+
+			this.removeSocket();
+			this.initialiseSocket();
+
+			// Requeue message
+			this.messageQueue.unshift(lastMsg);
+
+		}, this.TIMEOUT_MS);
+	}
+	sendMsg(msg){
+		if (!this.socket || this.socket.readyState === WebSocket.CLOSED) {
+			this.removeSocket();
+			this.initialiseSocket();
+			this.messageQueue.push(msg);
+			return;
+		}
+
+		if (this.socket.readyState === WebSocket.CONNECTING || this.isConnecting) {
+			this.messageQueue.push(msg);
+			return;
+		}
+
+		if (this.socket.readyState === WebSocket.OPEN) {
+			this.socket.send(msg);
+
+			// Start response timeout
+			this.startResponseTimeout(msg);
+		}
+	}
 
     SendCreateMessage(ComponentType, value){
         this.sendMsg("add component:"+ComponentType + "/"+value);
